688619dd162bd871db2c51bda82d2dc5286d3adc
Emmanuel KOUPOH <emmanuelkoupoh@gmail.com>
2023-05-30 17:35:12 +0000

    format

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 # logging.error('This is an error message')
 # logging.critical('This is a critical message')
 
+
 # Util Functions & Classes
 def loading(fp):
     with open(fp, "rb") as f:
@@ -24,14 +25,16 @@ def loading(fp):
     print(f"INFO: Loaded data : {data}")
     return data
 
+
 def predict(df, endpoint="simple"):
-    """Take a dataframe as input and use it to make predictions
-    """
-    
-    print(f"[Info] 'predict' function has been called through the endpoint '{endpoint}'. \n{df.to_markdown()}\n")
+    """Take a dataframe as input and use it to make predictions"""
+
+    print(
+        f"[Info] 'predict' function has been called through the endpoint '{endpoint}'. \n{df.to_markdown()}\n"
+    )
     logger.info(f"{df.to_markdown()}")
 
-    # scaling 
+    # scaling
     scaled_df = scaler.transform(df)
     print(f"INFO: Scaler output is of type {type(scaled_df)}")
 
@@ -49,21 +52,22 @@ def predict(df, endpoint="simple"):
     print(f"INFO: Highest probability indexes : {highest_proba_idx}")
 
     ## Maching prediction with classes
-    predicted_classes = [ labels[i] for i in highest_proba_idx ]
+    predicted_classes = [labels[i] for i in highest_proba_idx]
     print(f"INFO: Predicted classes : {predicted_classes}")
     # prediction[:, highest_proba_idx]
 
-    #save in df 
+    # save in df
     df["predicted proba"] = highest_proba
     df["predicted label"] = predicted_classes
 
     print(f"INFO: dataframe filled with prediction\n{df.to_markdown()}\n")
 
-    #parsing prediction
+    # parsing prediction
     # parsed = json.loads(df.to_json(orient="index")) # or
-    parsed = df.to_dict('records')
+    parsed = df.to_dict("records")
 
     return parsed
+
 
 ## INPUT MODELING
 class Land(BaseModel):
@@ -76,13 +80,15 @@ class Land(BaseModel):
     customer_age : int
     gender : Literal['male', 'female', 'other']
     """
-    N:float
-    P:float
-    K:float
-    temperature:float
-    humidity:float
-    ph:float
-    rainfall:float
+
+    N: float
+    P: float
+    K: float
+    temperature: float
+    humidity: float
+    ph: float
+    rainfall: float
+
 
 class Lands(BaseModel):
     inputs: List[Land]
@@ -109,42 +115,58 @@ labels = ml_objects["labels"]
 def root():
     return {"API": " This is an API to ... ."}
 
+
 @app.get("/checkup")
-def test(a:Optional[int], b:int):
-    return {"a":a, "b":b}
+def test(a: Optional[int], b: int):
+    return {"a": a, "b": b}
+
 
 ## ML endpoint
 @app.post("/predict")
-def make_prediction(N:float, P:float, K:float, temperature:float, humidity:float, ph:float, rainfall:float):
-    """Make prediction with the passed data
-    """
-    
+def make_prediction(
+    N: float,
+    P: float,
+    K: float,
+    temperature: float,
+    humidity: float,
+    ph: float,
+    rainfall: float,
+):
+    """Make prediction with the passed data"""
+
     df = pd.DataFrame(
         {
-            "N":[N], "P":[P], "K":[K], "temperature":[temperature], "humidity":[humidity], "ph":[ph], "rainfall":[rainfall]
+            "N": [N],
+            "P": [P],
+            "K": [K],
+            "temperature": [temperature],
+            "humidity": [humidity],
+            "ph": [ph],
+            "rainfall": [rainfall],
         }
     )
-    
-    parsed = predict(df=df) #df.to_dict('records')
+
+    parsed = predict(df=df)  # df.to_dict('records')
 
     return {
         "output": parsed,
     }
 
+
 @app.post("/predict_multi")
-def make_multi_prediction(multi_lands:Lands):
-    """Make prediction with the passed data
-    """
+def make_multi_prediction(multi_lands: Lands):
+    """Make prediction with the passed data"""
     print(f"Mutiple inputs passed: {multi_lands}\n")
     df = pd.DataFrame(multi_lands.return_list_of_dict())
-    
-    parsed = predict(df=df, endpoint="multi inputs") #df.to_dict('records')
+
+    parsed = predict(df=df, endpoint="multi inputs")  # df.to_dict('records')
 
     return {
         "output": parsed,
         "author": "Stella Archar",
         "api_version": ";)",
     }
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
